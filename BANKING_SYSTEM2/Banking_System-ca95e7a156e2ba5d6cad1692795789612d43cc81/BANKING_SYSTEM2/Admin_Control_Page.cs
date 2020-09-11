2b2a1c298db4a17cbdb28e61d2cf0ef0e921a863
krishna <krishna.senapati8249@gmail.com>
2020-09-11 17:26:16 +0530

    COMMITED FINAL

---
+++ BANKING_SYSTEM2/Banking_System-ca95e7a156e2ba5d6cad1692795789612d43cc81/BANKING_SYSTEM2/Admin_Control_Page.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BANKING_SYSTEM2
+{
+    public partial class Admin_Control_Page : Form
+    {
+        DBAccess objDbAccess = new DBAccess();
+        DataTable tbCustomer = new DataTable();
+        public Admin_Control_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Admin_Control_Page_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT * from CUSTOMER_REGISTRATION1";
+            objDbAccess.readDatathroughAdapter(query, tbCustomer);
+
+            dataGridView1.DataSource = tbCustomer;
+            objDbAccess.closeConn();
+
+        }
+   }
+
+
+}
